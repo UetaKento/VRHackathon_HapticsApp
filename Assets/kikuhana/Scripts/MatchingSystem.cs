b918cbbf9693309cd3424aabaf4066ffc5463610
kikka_osti <95869418+kikuhanaVR@users.noreply.github.com>
2023-12-23 22:31:00 +0900

    first commit

--- Assets/kikuhana/Scripts/MatchingSystem.cs
+++ Assets/kikuhana/Scripts/MatchingSystem.cs
@@ -4,31 +4,102 @@ using UnityEngine;
 
 public class MatchingSystem : MonoBehaviour
 {
-    public GameObject gameObject1;
-    public GameObject gameObject2;
-    bool fragg001;
-    bool fragg002;
+    bool triggerL;
+    bool triggerR;
+    string hapticsNum1;
+    string hapticsNum2;
     
     // Start is called before the first frame update
     void Start()
     {
-        fragg001 = false;
-        fragg002 = false;
+        triggerL = false;
+        triggerR = false;
+    }
+
+    private void FixedUpdate()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+        {
+            Debug.Log("Trigger:L");
+            triggerL = true;
+        }
+        if (OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger))
+        {
+            triggerL = false;
+        }
+        if (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger))
+        {
+            Debug.Log("Trigger;R");
+            triggerR = true;
+        }
+        if (OVRInput.GetUp(OVRInput.Button.SecondaryIndexTrigger))
+        {
+            triggerR = false;
+        }
+        if ((OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger) && OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger)) 
+                || (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger) && OVRInput.Get(OVRInput.Button.SecondaryIndexTrigger)))
+        {
+            Judgement();
+            Debug.Log("Judgement");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Debug.Log("select is " + selectX  + " and " + selectY);
+    }
+
+    public void Judgement()
+    {
+        if (hapticsNum1 == hapticsNum2)
+        {
+           Correct();
+        }
+        if (hapticsNum1 != hapticsNum2)
+        {
+            Failure();
+        }
+    }
+
+    public void Correct()
+    {
+        Debug.Log("Correct! :)");
+    }
+
+    public void Failure()
+    {
+        Debug.Log("Failure... :(");
+    }
+
+    public void Select(string numOfHuptics)
+    {
+        Debug.Log("SERECT");
         
+
+        if (hapticsNum1 != null)
+        { 
+            hapticsNum2 = numOfHuptics; 
+        }
+        else
+        {
+            hapticsNum1 = numOfHuptics;
+        }
+
     }
 
-    public void FraggChange001(bool swt)
+    public void UnSelect()
     {
+        Debug.Log("UNSELECT");
 
-    }
-
-    public void FraggChange002(bool swt)
-    {
+        if (hapticsNum2 == null)
+        {
+            hapticsNum1 = null;
+        }
+        else
+        {
+            hapticsNum2 = null;
+        }
 
     }
 }
