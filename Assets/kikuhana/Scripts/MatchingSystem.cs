84ef3c05a09430ba65f835fcd09cfed48e1c3e69
kikka_osti <95869418+kikuhanaVR@users.noreply.github.com>
2023-12-24 01:08:41 +0900

    add haptics

--- Assets/kikuhana/Scripts/MatchingSystem.cs
+++ Assets/kikuhana/Scripts/MatchingSystem.cs
@@ -1,26 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Oculus.Haptics;
+
 
 public class MatchingSystem : MonoBehaviour
 {
+    public GameObject[] gameObjects;
+    string[] TagNames;
     bool triggerL;
     bool triggerR;
+    bool handtriggerL;
+    bool handtriggerR;
     string hapticsNum1;
     string hapticsNum2;
-    
+    public Transform clearTransform;
+    Vector3 clearPosition;
+
+    public HapticClip clip1;
+    private HapticClipPlayer player;
+
     // Start is called before the first frame update
     void Start()
     {
+        player = new HapticClipPlayer(clip1);
         triggerL = false;
         triggerR = false;
+        clearPosition = clearTransform.position;
     }
 
     private void FixedUpdate()
     {
         if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
         {
-            Debug.Log("Trigger:L");
+            //Debug.Log("Trigger:L");
             triggerL = true;
         }
         if (OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger))
@@ -29,13 +42,26 @@ public class MatchingSystem : MonoBehaviour
         }
         if (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger))
         {
-            Debug.Log("Trigger;R");
+            //Debug.Log("Trigger;R");
             triggerR = true;
         }
         if (OVRInput.GetUp(OVRInput.Button.SecondaryIndexTrigger))
         {
             triggerR = false;
         }
+
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger))
+        {
+            Debug.Log("HandTrigger:L");
+            handtriggerL = true;
+        }
+        if (OVRInput.GetDown(OVRInput.Button.SecondaryHandTrigger))
+        {
+            Debug.Log("HandTrigger:R");
+            handtriggerR = true;
+        }
+
+
         if ((OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger) && OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger)) 
                 || (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger) && OVRInput.Get(OVRInput.Button.SecondaryIndexTrigger)))
         {
@@ -50,11 +76,27 @@ public class MatchingSystem : MonoBehaviour
         //Debug.Log("select is " + selectX  + " and " + selectY);
     }
 
+    public void Haptics(HapticClip hapticClip)
+    {
+        player.clip = hapticClip;
+        //player.Play(Controller.Left);
+        if(handtriggerL)
+        {
+            player.Play(Controller.Left);
+            handtriggerL = false;
+        }
+        if(handtriggerR)
+        {
+            player.Play(Controller.Right);
+            handtriggerR = false;
+        }
+    }
+
     public void Judgement()
     {
         if (hapticsNum1 == hapticsNum2)
         {
-           Correct();
+            Correct();
         }
         if (hapticsNum1 != hapticsNum2)
         {
@@ -65,6 +107,10 @@ public class MatchingSystem : MonoBehaviour
     public void Correct()
     {
         Debug.Log("Correct! :)");
+        foreach (GameObject gameObject in gameObjects)
+        {
+            //if(gameObject)
+        }
     }
 
     public void Failure()
